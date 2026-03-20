60e44b1c360e83977a82700500bc4c026198c5cd
박기순 <ks.park@sqisoft.com>
2026-03-20 11:12:54 +0900

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <></>;
+  const [userInfo, setUserInfo] = useState({
+    name: "Jack",
+    age: 26,
+    gender: "male",
+  });
+  const handleUpdateUserInfo = () => {
+    setUserInfo({
+      ...userInfo,
+      name: "Keesoon",
+    });
+  };
+  return (
+    <>
+      <p>name: {userInfo.name}</p>
+      <p>age: {userInfo.age}</p>
+      <p>gender: {userInfo.gender}</p>
+      <Button onClick={handleUpdateUserInfo}>Update UserInfo</Button>
+    </>
+  );
 };
 export default App;
